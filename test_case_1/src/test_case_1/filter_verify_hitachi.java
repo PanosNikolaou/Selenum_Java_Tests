9e2621f7606167937ab40771cc1dbc11b2e0d26a
Panos Nikolaou <38185286+PanosNikolaou@users.noreply.github.com>
2021-07-01 09:57:06 +0200

    code formatting

--- test_case_1/src/test_case_1/filter_verify_hitachi.java
+++ test_case_1/src/test_case_1/filter_verify_hitachi.java
@@ -14,79 +14,79 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
- 
-public class filter_verify_hitachi{
 
-	//definition of the pause function
-    public static void pause(Integer milliseconds){
-    try {
-        TimeUnit.MILLISECONDS.sleep(milliseconds);
-    } catch (InterruptedException e) {
-        e.printStackTrace();
+public class filter_verify_hitachi {
+
+    //definition of the pause function
+    public static void pause(Integer milliseconds) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+
+        //Initiating your chromedriver
+        WebDriver driver = new ChromeDriver();
+
+        //Applied wait time
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        //maximize window
+        driver.manage().window().maximize();
+
+        //open browser with the desired URL
+        driver.get("https://www.alza.cz");
+
+        //find the language selector
+        WebElement selector = driver.findElement(By.className("selector"));
+
+        //click on the selector for the options
+        selector.click();
+
+        //get the link for the english language
+        WebElement language = driver.findElement(By.className("langEN"));
+
+        //click on the link
+        language.click();
+
+        //store to a variable the search element
+        WebElement searchBar = driver.findElement(By.id("edtSearch"));
+
+        //find the button to submit the search
+        WebElement hledat = driver.findElement(By.id("btnSearch"));
+
+        //type Hitahci in the search bar
+        searchBar.sendKeys("Hitachi");
+
+        //click on the search button
+        hledat.click();
+
+        // Filter by Hitachi
+        WebElement producer = driver.findElement(By.id("producer-1422"));
+        producer.findElement(By.className("valueInputContainer")).click();
+
+        //use of the function pause until to get the results for the filtering
+        pause(3000);
+
+        //store in a list all the leements that contain the text HITACHI in their texts
+        List < WebElement > allProduct = driver.findElements(By.xpath("//*[contains(text(),'HITACHI ')]"));
+
+        //print out the text of the elements in the list
+        for (WebElement product: allProduct) {
+            System.out.println(product.getText());
+        }
+
+        //verify that the number of the filtered elements equals to the number of the list elements 
+        String resultsNumber = driver.findElement(By.id("lblNumberItem")).getText();
+        Assert.assertEquals(allProduct.size(), Integer.parseInt(resultsNumber));
+
+        //close the driver
+        driver.close();
+
     }
-    
-	public static void main(String[] args) {
-
-	System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-	
-	//Initiating your chromedriver
-	WebDriver driver=new ChromeDriver();
-	
-	//Applied wait time
-	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-	//maximize window
-	driver.manage().window().maximize();
-
-	//open browser with the desired URL
-	driver.get("https://www.alza.cz");
-
-	//find the language selector
-	WebElement selector = driver.findElement(By.className("selector"));
-	
-	//click on the selector for the options
-	selector.click();
-	
-	//get the link for the english language
-    WebElement language = driver.findElement(By.className("langEN"));
-	
-    //click on the link
-    language.click();
-    
-    //store to a variable the search element
-	WebElement searchBar = driver.findElement(By.id("edtSearch"));
-	
-	//find the button to submit the search
-    WebElement hledat = driver.findElement(By.id("btnSearch"));
-	
-    //type Hitahci in the search bar
-	searchBar.sendKeys("Hitachi");
-	
-	//click on the search button
-	hledat.click();
-	
-	// Filter by Hitachi
-	WebElement producer = driver.findElement(By.id("producer-1422"));
-	producer.findElement(By.className("valueInputContainer")).click();
-    
-	//use of the function pause until to get the results for the filtering
-	pause(3000);
-    
-	//store in a list all the leements that contain the text HITACHI in their texts
-    List <WebElement> allProduct = driver.findElements(By.xpath("//*[contains(text(),'HITACHI ')]"));
-    
-    //print out the text of the elements in the list
-	for(WebElement product : allProduct){
-		System.out.println(product.getText());
-	}
-
-	//verify that the number of the filtered elements equals to the number of the list elements 
-    String resultsNumber = driver.findElement(By.id("lblNumberItem")).getText();
-    Assert.assertEquals(allProduct.size(), Integer.parseInt(resultsNumber));
-
-    //close the driver
-    driver.close();
-    
-	}
 }
--- test_case_2/src/test_case_2/verify_menu_languages.java
+++ test_case_2/src/test_case_2/verify_menu_languages.java
@@ -15,205 +15,205 @@ import org.openqa.selenium.WebElement;
 
 
 public class verify_menu_languages {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-	//setting the driver executable
-	System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        //setting the driver executable
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 
-	//Initiating your chromedriver
-	WebDriver driver=new ChromeDriver();
-	
-	//Applied wait time
-	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //Initiating your chromedriver
+        WebDriver driver = new ChromeDriver();
 
-	//maximize window
-	driver.manage().window().maximize();
+        //Applied wait time
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-	//open browser with the desired URL
-	driver.get("https://www.alza.cz");
-	
-	//find the selector
-	WebElement selector = driver.findElement(By.className("selector"));
+        //maximize window
+        driver.manage().window().maximize();
 
-	//click on selector
-	selector.click();
-	
-	//find the link for the english language
-    WebElement language_EN = driver.findElement(By.className("langEN"));
-	
-    //click on the link to switch the language to english
-    language_EN.click();
-    
-    // Verify the actual url is the expected url for language EN
-    String actualUrl="https://www.alza.cz/EN/";
-    String expectedUrl= driver.getCurrentUrl();
-    Assert.assertEquals(expectedUrl, actualUrl);
+        //open browser with the desired URL
+        driver.get("https://www.alza.cz");
 
-    //find the selector
-	WebElement selector2 = driver.findElement(By.className("selector"));
-	
-	//click on the selector
-	selector2.click();
-	
-	//find the link for language SK
-    WebElement languageSK = driver.findElement(By.className("langSK"));
+        //find the selector
+        WebElement selector = driver.findElement(By.className("selector"));
 
-    //click to change to language
-    languageSK.click();
+        //click on selector
+        selector.click();
 
-    // Verify the actual url is the expected url for language SK
-    String actualUrl2="https://www.alza.sk/";
-    String expectedUrl2= driver.getCurrentUrl();
-    Assert.assertEquals(expectedUrl2, actualUrl2);
-    
-    //find the selector
-    WebElement country_selector_2 = driver.findElement(By.className("selector"));
-    
-    //click to expand the options
-    country_selector_2.click();
-    
-    // Get the link for Croatia
-    WebElement country_selection2 = driver.findElement(By.linkText("Croatia"));
-    
-    // click on the country link for Croatia
-    country_selection2.click();
-    
-    //compare the the actual url is the expected url for country equals to Croatia
-    String actualUrl_HR="https://www.alzashop.com/?";
-    String expectedUr_HR= driver.getCurrentUrl();
-    Assert.assertEquals(actualUrl_HR, expectedUr_HR);
-    
-    //compare the text for shipping to thats its equal to Croatia
-    String expected_shipping_country_HR="Croatia";
-    String actual_shipping_country_HR= driver.findElement(By.className("shiptoTxt")).getText();
-    Assert.assertEquals(expected_shipping_country_HR, actual_shipping_country_HR);
-    
-    //redirect to alza.cz
-    driver.get("https://www.alza.cz");
-    
-    //find the selector
-    WebElement country_selector_3 = driver.findElement(By.className("selector"));
-    
-    //click on the selector to expand the options
-    country_selector_3.click();
-    
-    //find the link for the country France
-    WebElement country_selection3 = driver.findElement(By.linkText("France"));
-    
-    //click on the link to switch to the french web site
-    country_selection3.click();
-    
-    //test that the expected url is the defined url
-    String actualUrl_FR = "https://www.alzashop.com/?";
-    String expectedUr_FR =  driver.getCurrentUrl();
-    Assert.assertEquals(actualUrl_FR, expectedUr_FR);
-    
-    //compare the text for shipping to thats its equal to France
-    String expected_shipping_country_FR = "France";
-    String actual_shipping_country_FR = driver.findElement(By.className("shiptoTxt")).getText();
-    Assert.assertEquals(expected_shipping_country_FR, actual_shipping_country_FR);
-    
-    //redirect to alza.cz    
-    driver.get("https://www.alza.cz");
-    
-    //find the selector 
-	driver.findElement(By.className("selector")).click();
-    
-	//click on the link for the country Austria
-    driver.findElement(By.linkText("Austria")).click();
-    
-    //test that the expected url is the defined url
-    String actualUrl_AT="https://www.alza.at/";
-    String expectedUr_AT= driver.getCurrentUrl();
-    Assert.assertEquals(actualUrl_AT,expectedUr_AT);
-    
-    //redirect to alza.cz
-    driver.get("https://www.alza.cz");
-    
-    //find the selector 
-	driver.findElement(By.className("selector")).click();
-	
-    //click on the country link for Germany
-    driver.findElement(By.linkText("Germany")).click();
+        //find the link for the english language
+        WebElement language_EN = driver.findElement(By.className("langEN"));
 
-    //test that the expected url is the defined url
-    String actualUrl_DE="https://www.alza.de/";
-    String expectedUr_DE= driver.getCurrentUrl();
-    Assert.assertEquals(actualUrl_DE, expectedUr_DE);
+        //click on the link to switch the language to english
+        language_EN.click();
 
-    //redirect to alza.cz
-    driver.get("https://www.alza.cz");
-    
-    //find the selector 
-	driver.findElement(By.className("selector")).click();
+        // Verify the actual url is the expected url for language EN
+        String actualUrl = "https://www.alza.cz/EN/";
+        String expectedUrl = driver.getCurrentUrl();
+        Assert.assertEquals(expectedUrl, actualUrl);
 
-    //click on the country link for Hungary
-    driver.findElement(By.linkText("Hungary")).click();
+        //find the selector
+        WebElement selector2 = driver.findElement(By.className("selector"));
 
-    //test that the expected url is the defined url
-    String actualUrl_HU="https://www.alza.hu/";
-    String expectedUr_HU= driver.getCurrentUrl();
-    Assert.assertEquals(actualUrl_HU, expectedUr_HU);
-    
-    //redirect to alza.cz
-    driver.get("https://www.alza.cz");
-    
-    //find the selector 
-	driver.findElement(By.className("selector")).click();
+        //click on the selector
+        selector2.click();
 
-    // click on the country link for Poland
-    driver.findElement(By.linkText("Poland")).click();
+        //find the link for language SK
+        WebElement languageSK = driver.findElement(By.className("langSK"));
 
-    //test that the expected url is the defined url
-    String actualUrl_PL = "https://www.alzashop.com/?";
-    String expectedUr_PL = driver.getCurrentUrl();
-    Assert.assertEquals(actualUrl_PL, expectedUr_PL);
-    
-    //redirect to alza.cz
-    driver.get("https://www.alza.cz");
-    
-    //find the selector
-	driver.findElement(By.className("selector")).click();
+        //click to change to language
+        languageSK.click();
 
-	//click on the country link for Romania
-    driver.findElement(By.linkText("Romania")).click();
+        // Verify the actual url is the expected url for language SK
+        String actualUrl2 = "https://www.alza.sk/";
+        String expectedUrl2 = driver.getCurrentUrl();
+        Assert.assertEquals(expectedUrl2, actualUrl2);
 
-    //test that the expected url is the defined url
-    String actualUrl_RO ="https://www.alzashop.com/?";
-    String expectedUr_RO = driver.getCurrentUrl();
-    Assert.assertEquals(actualUrl_RO, expectedUr_RO);
-    
-    //redirect to alza.cz
-    driver.get("https://www.alza.cz");
-    
-    //find the selector 
-	driver.findElement(By.className("selector")).click();
-	
-    //click on the country link for Slovenia
-    driver.findElement(By.linkText("Slovenia")).click();
+        //find the selector
+        WebElement country_selector_2 = driver.findElement(By.className("selector"));
 
-    //test that the expected url is the defined url
-    String actualUrl_SLO ="https://www.alzashop.com/?";
-    String expectedUr_SLO = driver.getCurrentUrl();
-    Assert.assertEquals(actualUrl_SLO, expectedUr_SLO);
-    
-    //redirect to alza.cz
-    driver.get("https://www.alza.cz");
-    
-    //find the selector
-	driver.findElement(By.className("selector")).click();
+        //click to expand the options
+        country_selector_2.click();
 
-    //click on the country link for United Kingdom
-    driver.findElement(By.linkText("United Kingdom")).click();
+        // Get the link for Croatia
+        WebElement country_selection2 = driver.findElement(By.linkText("Croatia"));
 
-    //test that the expected url is the defined url
-    String actualUrl_UK ="https://www.alza.co.uk/";
-    String expectedUr_UK = driver.getCurrentUrl();
-    Assert.assertEquals(actualUrl_UK, expectedUr_UK);
-    
-    //close the driver
-    driver.close();
-    
-	}
+        // click on the country link for Croatia
+        country_selection2.click();
+
+        //compare the the actual url is the expected url for country equals to Croatia
+        String actualUrl_HR = "https://www.alzashop.com/?";
+        String expectedUr_HR = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl_HR, expectedUr_HR);
+
+        //compare the text for shipping to thats its equal to Croatia
+        String expected_shipping_country_HR = "Croatia";
+        String actual_shipping_country_HR = driver.findElement(By.className("shiptoTxt")).getText();
+        Assert.assertEquals(expected_shipping_country_HR, actual_shipping_country_HR);
+
+        //redirect to alza.cz
+        driver.get("https://www.alza.cz");
+
+        //find the selector
+        WebElement country_selector_3 = driver.findElement(By.className("selector"));
+
+        //click on the selector to expand the options
+        country_selector_3.click();
+
+        //find the link for the country France
+        WebElement country_selection3 = driver.findElement(By.linkText("France"));
+
+        //click on the link to switch to the french web site
+        country_selection3.click();
+
+        //test that the expected url is the defined url
+        String actualUrl_FR = "https://www.alzashop.com/?";
+        String expectedUr_FR = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl_FR, expectedUr_FR);
+
+        //compare the text for shipping to thats its equal to France
+        String expected_shipping_country_FR = "France";
+        String actual_shipping_country_FR = driver.findElement(By.className("shiptoTxt")).getText();
+        Assert.assertEquals(expected_shipping_country_FR, actual_shipping_country_FR);
+
+        //redirect to alza.cz    
+        driver.get("https://www.alza.cz");
+
+        //find the selector 
+        driver.findElement(By.className("selector")).click();
+
+        //click on the link for the country Austria
+        driver.findElement(By.linkText("Austria")).click();
+
+        //test that the expected url is the defined url
+        String actualUrl_AT = "https://www.alza.at/";
+        String expectedUr_AT = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl_AT, expectedUr_AT);
+
+        //redirect to alza.cz
+        driver.get("https://www.alza.cz");
+
+        //find the selector 
+        driver.findElement(By.className("selector")).click();
+
+        //click on the country link for Germany
+        driver.findElement(By.linkText("Germany")).click();
+
+        //test that the expected url is the defined url
+        String actualUrl_DE = "https://www.alza.de/";
+        String expectedUr_DE = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl_DE, expectedUr_DE);
+
+        //redirect to alza.cz
+        driver.get("https://www.alza.cz");
+
+        //find the selector 
+        driver.findElement(By.className("selector")).click();
+
+        //click on the country link for Hungary
+        driver.findElement(By.linkText("Hungary")).click();
+
+        //test that the expected url is the defined url
+        String actualUrl_HU = "https://www.alza.hu/";
+        String expectedUr_HU = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl_HU, expectedUr_HU);
+
+        //redirect to alza.cz
+        driver.get("https://www.alza.cz");
+
+        //find the selector 
+        driver.findElement(By.className("selector")).click();
+
+        // click on the country link for Poland
+        driver.findElement(By.linkText("Poland")).click();
+
+        //test that the expected url is the defined url
+        String actualUrl_PL = "https://www.alzashop.com/?";
+        String expectedUr_PL = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl_PL, expectedUr_PL);
+
+        //redirect to alza.cz
+        driver.get("https://www.alza.cz");
+
+        //find the selector
+        driver.findElement(By.className("selector")).click();
+
+        //click on the country link for Romania
+        driver.findElement(By.linkText("Romania")).click();
+
+        //test that the expected url is the defined url
+        String actualUrl_RO = "https://www.alzashop.com/?";
+        String expectedUr_RO = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl_RO, expectedUr_RO);
+
+        //redirect to alza.cz
+        driver.get("https://www.alza.cz");
+
+        //find the selector 
+        driver.findElement(By.className("selector")).click();
+
+        //click on the country link for Slovenia
+        driver.findElement(By.linkText("Slovenia")).click();
+
+        //test that the expected url is the defined url
+        String actualUrl_SLO = "https://www.alzashop.com/?";
+        String expectedUr_SLO = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl_SLO, expectedUr_SLO);
+
+        //redirect to alza.cz
+        driver.get("https://www.alza.cz");
+
+        //find the selector
+        driver.findElement(By.className("selector")).click();
+
+        //click on the country link for United Kingdom
+        driver.findElement(By.linkText("United Kingdom")).click();
+
+        //test that the expected url is the defined url
+        String actualUrl_UK = "https://www.alza.co.uk/";
+        String expectedUr_UK = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl_UK, expectedUr_UK);
+
+        //close the driver
+        driver.close();
+
+    }
 
 }
